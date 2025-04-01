9e9a37b4694eb5e3e53c81142b941bad7d78eb48
glsl7503 <gim325901@gmail.com>
2025-04-01 17:19:33 +0900

    todo 기본 구조 잡기

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// EJS 설정
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// 정적 파일 제공 (public 폴더 사용)
+app.use(express.static(path.join(__dirname, '../public'))); // public 폴더에 있는 CSS, JS 파일 등 제공
+
+// 서버 실행, 8080번 포트에서 대기
+app.listen(8080, function() {
+    console.log('Listening on port 8080');
+});
+
+app.get('/', function(req, res) {
+    res.render('index'); // EJS 템플릿 파일 렌더링
+});
+
+app.get('/todo', function(req, res) {
+    res.render('todo'); 
+});
